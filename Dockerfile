a89e15c0e9808ea3109ea7d4ffccdf85a95fd7aa
Roger Garcia <roger.garcia@guifi.net>
2017-09-26 11:58:02 +0200

    New implementation: - Base image stretch - PHP 7.0 - creating dedicated user

---
+++ Dockerfile
@@ -0,0 +1,34 @@
+# Set the base image
+FROM debian:stretch
+
+LABEL net.guifi.vendor="Fundacio guifi.net"
+LABEL version="1.0"
+LABEL description="This docker image is ready for \
+developing with guifimaps services"
+LABEL maintainer="roger.garcia@guifi.net"
+
+
+RUN apt-get update && apt-get dist-upgrade -y \
+  && apt-get install -y apache2 php7.0 \
+  php7.0-xml wget gosu \
+  git cron \
+  && apt-get clean \
+  && apt-get autoremove \
+  && rm -rf /var/lib/apt/lists/*
+
+# Preparing development dir
+RUN mkdir /usr/share/guifimaps/
+
+# Creating UNIX User for fiberfy (security reasons)
+RUN groupadd --system $GMAPS_UNIX_USER && useradd --system --gid $GMAPS_UNIX_USER $GMAPS_UNIX_USER --create-home
+
+# Setting Volume
+VOLUME /usr/share/guifimaps/
+
+# Copying Entrypoint scripts
+COPY ./docker-entrypoint.sh /
+COPY ./guifimaps-entry.pl /
+
+EXPOSE 80
+
+ENTRYPOINT ["/docker-entrypoint.sh"]
---
+++ guifimaps-entry.pl
@@ -0,0 +1,48 @@
+#!/usr/bin/perl
+# This script configures fiberfy for first time
+#
+use warnings;
+use strict;
+
+
+print "Checking configurations...\n";
+
+if (! -e "INSTALLED") {
+
+    my $output = `rm -rf {,.[!.],..?}*`;
+    if ($? != 0) {
+        # Error
+        die "Error erasing all volume.\n";
+    }
+
+    $output = `git init && git remote add origin https://github.com/guifi/guifimaps.git \\
+                && git fetch && git checkout -t origin/master`;
+    if ($? != 0) {
+        # Error
+        die "Error getting code from git repository.\n";
+    }
+
+    $output = `chown -R $ENV{GMAPS_UNIX_USER}:$ENV{GMAPS_UNIX_USER} .`;
+    if ($? != 0) {
+        # Error
+        die "Error changing permissions.\n";
+    }
+
+    $output = `gosu $ENV{GMAPS_UNIX_USER} `;
+    if ($? != 0) {
+        # Error
+        die "Error installing node deps.\n";
+    }
+
+    # make INSTALLED file
+    $output = `touch INSTALLED`;
+    if ($? != 0) {
+     # Error
+     die "Error creating INSTALLED file.\n";
+    }
+
+    print "Guifi.net guifimaps successfully installed in Docker image!\n";
+}
+else {
+    print "Already installed.\n";
+}
