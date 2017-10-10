d2f566647a3f52a4595107de4c83872ecc28d08a
Roger Garcia <roger.garcia@guifi.net>
2017-10-10 12:12:32 +0200

    End of main implementation. gmaps user hardcoded (need to review)

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/env bash
-#cd /usr/share/drupal/guifi-web
-#drush si -y --site-name=guifi.net --db-url=mysqli://guifi:guifi@database/guifidev --account-name=admin --account-pass=drupal
 # Configuration script in Perl
+cron start
 perl /guifimaps-entry.pl
+gosu gmaps bash -c '( crontab -l ; echo "3,18,33,48 * * * *  cd /usr/share/guifimaps; php refresh.php > /dev/null 2>&1; cd data; rm -rf dlinks/*; rm -rf dnodes/*; ogr2ogr -f "ESRI Shapefile" dlinks dlinks.gml; ogr2ogr -f "ESRI Shapefile" dnodes dnodes.gml
+" ) | crontab -'
 /usr/sbin/apache2ctl -D FOREGROUND
--- guifimaps-entry.pl
+++ guifimaps-entry.pl
@@ -22,17 +22,10 @@ if (! -e "INSTALLED") {
         die "Error getting code from git repository.\n";
     }
 
-    $output = `chown -R $ENV{GMAPS_UNIX_USER}:www-data .`;
+    $output = `chown -R $ENV{GMAPS_UNIX_USER}:www-data . && chown -R $ENV{GMAPS_UNIX_USER}:www-data ./data`;
     if ($? != 0) {
         # Error
         die "Error changing permissions.\n";
-    }
-
-    $output = `gosu $ENV{GMAPS_UNIX_USER} bash -c '( crontab -l ; echo "3,18,33,48 * * * *  cd /usr/share/guifimaps; php refresh.php > /dev/null 2>&1; cd data; rm -rf dlinks/*; rm -rf dnodes/*; ogr2ogr -f "ESRI Shapefile" dlinks dlinks.gml; ogr2ogr -f "ESRI Shapefile" dnodes dnodes.gml
-" ) | crontab -'`;
-    if ($? != 0) {
-        # Error
-        die "Error installing cron command.\n";
     }
 
     # make INSTALLED file
